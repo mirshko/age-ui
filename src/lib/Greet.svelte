a590e75c7356a0bac76be64ccd605352d840f1af
Jeff Reiner <theundeadghost@gmail.com>
2023-07-09 13:58:20 +0200

    Remove boilerplate

--- src/lib/Greet.svelte
+++ src/lib/Greet.svelte
@@ -1,19 +1,20 @@
 <script lang="ts">
-  import { invoke } from "@tauri-apps/api/tauri"
+  import { invoke } from "@tauri-apps/api/tauri";
 
   let name = "";
-  let greetMsg = ""
+  let greetMsg = "";
 
-  async function greet(){
+  async function greet() {
     // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    greetMsg = await invoke("greet", { name })
+    greetMsg = await invoke("greet", { name });
   }
 </script>
 
 <div>
-  <form class="row" on:submit|preventDefault={greet}>
+  <form on:submit|preventDefault={greet}>
     <input id="greet-input" placeholder="Enter a name..." bind:value={name} />
     <button type="submit">Greet</button>
   </form>
+
   <p>{greetMsg}</p>
 </div>
