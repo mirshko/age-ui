fc73055f74f5fa86cf0baebd51a9221a6bd3878a
Jeff Reiner <theundeadghost@gmail.com>
2023-07-09 15:11:20 +0200

    Hook up Tabs

--- src/App.svelte
+++ src/App.svelte
@@ -1,45 +1,26 @@
 <script lang="ts">
   import Greet from "./lib/Greet.svelte";
+  import { Tabs, TabList, TabPanel, Tab } from "./lib/tabs";
 </script>
 
 <main class="p-5">
-  <Greet />
+  <Tabs>
+    <TabList>
+      <Tab>Identities</Tab>
+      <Tab>Encrypt</Tab>
+      <Tab>Decrypt</Tab>
+    </TabList>
 
-  <nav
-    class="isolate flex divide-x divide-gray-200 rounded-lg shadow fixed bottom-5 inset-x-5"
-    aria-label="Tabs"
-  >
-    <!-- Current: "text-gray-900", Default: "text-gray-500 hover:text-gray-700" -->
-    <a
-      href="#"
-      class="text-gray-900 rounded-l-lg group relative min-w-0 flex-1 overflow-hidden bg-white py-4 px-4 text-center text-sm font-medium hover:bg-gray-50 focus:z-10"
-      aria-current="page"
-    >
-      <span>Identities</span>
-      <span
-        aria-hidden="true"
-        class="bg-indigo-500 absolute inset-x-0 bottom-0 h-0.5"
-      />
-    </a>
-    <a
-      href="#"
-      class="text-gray-500 hover:text-gray-700 group relative min-w-0 flex-1 overflow-hidden bg-white py-4 px-4 text-center text-sm font-medium hover:bg-gray-50 focus:z-10"
-    >
-      <span>Encrypt</span>
-      <span
-        aria-hidden="true"
-        class="bg-transparent absolute inset-x-0 bottom-0 h-0.5"
-      />
-    </a>
-    <a
-      href="#"
-      class="text-gray-500 hover:text-gray-700 rounded-r-lg group relative min-w-0 flex-1 overflow-hidden bg-white py-4 px-4 text-center text-sm font-medium hover:bg-gray-50 focus:z-10"
-    >
-      <span>Decrypt</span>
-      <span
-        aria-hidden="true"
-        class="bg-transparent absolute inset-x-0 bottom-0 h-0.5"
-      />
-    </a>
-  </nav>
+    <TabPanel>
+      <Greet />
+    </TabPanel>
+
+    <TabPanel>
+      <h2>Second panel</h2>
+    </TabPanel>
+
+    <TabPanel>
+      <h2>Third panel</h2>
+    </TabPanel>
+  </Tabs>
 </main>
---
+++ src/lib/TabList.svelte
@@ -0,0 +1,6 @@
+<nav
+  class="isolate flex divide-x divide-gray-200 rounded-lg shadow fixed bottom-5 inset-x-5"
+  aria-label="Tabs"
+>
+  <slot />
+</nav>
