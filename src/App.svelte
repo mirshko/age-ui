d484ab83025dae0b5d50a41ef8375a8528bafc0b
Jeff Reiner <theundeadghost@gmail.com>
2023-07-09 14:51:36 +0200

    Stub in tabs ui

--- src/App.svelte
+++ src/App.svelte
@@ -2,6 +2,44 @@
   import Greet from "./lib/Greet.svelte";
 </script>
 
-<main class="p-5">
+<main class="p-5 pt-10">
   <Greet />
+
+  <nav
+    class="isolate flex divide-x divide-gray-200 rounded-lg shadow fixed bottom-5 inset-x-5"
+    aria-label="Tabs"
+  >
+    <!-- Current: "text-gray-900", Default: "text-gray-500 hover:text-gray-700" -->
+    <a
+      href="#"
+      class="text-gray-900 rounded-l-lg group relative min-w-0 flex-1 overflow-hidden bg-white py-4 px-4 text-center text-sm font-medium hover:bg-gray-50 focus:z-10"
+      aria-current="page"
+    >
+      <span>Identities</span>
+      <span
+        aria-hidden="true"
+        class="bg-indigo-500 absolute inset-x-0 bottom-0 h-0.5"
+      />
+    </a>
+    <a
+      href="#"
+      class="text-gray-500 hover:text-gray-700 group relative min-w-0 flex-1 overflow-hidden bg-white py-4 px-4 text-center text-sm font-medium hover:bg-gray-50 focus:z-10"
+    >
+      <span>Encrypt</span>
+      <span
+        aria-hidden="true"
+        class="bg-transparent absolute inset-x-0 bottom-0 h-0.5"
+      />
+    </a>
+    <a
+      href="#"
+      class="text-gray-500 hover:text-gray-700 rounded-r-lg group relative min-w-0 flex-1 overflow-hidden bg-white py-4 px-4 text-center text-sm font-medium hover:bg-gray-50 focus:z-10"
+    >
+      <span>Decrypt</span>
+      <span
+        aria-hidden="true"
+        class="bg-transparent absolute inset-x-0 bottom-0 h-0.5"
+      />
+    </a>
+  </nav>
 </main>
