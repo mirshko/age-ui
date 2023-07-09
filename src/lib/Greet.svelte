5c1ddd7db6072be9de365bdac637eff3fc80c3e2
Jeff Reiner <theundeadghost@gmail.com>
2023-07-09 14:31:50 +0200

    Add tailwindcss/forms plugin

--- src/lib/Greet.svelte
+++ src/lib/Greet.svelte
@@ -12,8 +12,23 @@
 
 <div>
   <form on:submit|preventDefault={greet}>
-    <input id="greet-input" placeholder="Enter a name..." bind:value={name} />
-    <button type="submit">Greet</button>
+    <div class="flex gap-2 max-w-xs">
+      <input
+        id="greet-input"
+        placeholder="Enter a name..."
+        required
+        bind:value={name}
+        type="text"
+        class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+      />
+
+      <button
+        type="submit"
+        class="rounded-md bg-indigo-600 px-2.5 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+      >
+        Greet
+      </button>
+    </div>
   </form>
 
   <p>{greetMsg}</p>
