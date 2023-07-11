4034baaf7ec9b1042bdc0b68d130267a9748b5fe
Jeff Reiner <theundeadghost@gmail.com>
2023-07-11 19:50:49 +0200

    Refactor tabs to melt-ui

--- src/App.svelte
+++ src/App.svelte
@@ -1,28 +1,50 @@
 <script lang="ts">
-  import { Tab, TabList, TabPanel, Tabs } from "./lib/tabs";
+  import { createTabs } from "@melt-ui/svelte";
   import Decrypt from "./screens/Decrypt.svelte";
   import Encrypt from "./screens/Encrypt.svelte";
   import Identities from "./screens/Identities.svelte";
+
+  const { root, list, content, trigger } = createTabs({
+    value: "Identities",
+  });
 </script>
 
 <main class="p-5">
-  <Tabs>
-    <TabList>
-      <Tab>Identities</Tab>
-      <Tab>Encrypt</Tab>
-      <Tab>Decrypt</Tab>
-    </TabList>
+  <div {...$root}>
+    <div
+      class="fixed bottom-0 inset-x-0 px-5 pb-5 from-transparent to-gray-100 bg-gradient-to-b"
+    >
+      <nav
+        class="isolate flex divide-x divide-gray-200 rounded-lg shadow"
+        {...$list}
+      >
+        <button {...$trigger("Identities")} use:trigger class="trigger">
+          Identities
+          <span aria-hidden="true" class="trigger-indicator" />
+        </button>
 
-    <TabPanel>
+        <button {...$trigger("Encrypt")} use:trigger class="trigger">
+          Encrypt
+          <span aria-hidden="true" class="trigger-indicator" />
+        </button>
+
+        <button {...$trigger("Decrypt")} use:trigger class="trigger">
+          Decrypt
+          <span aria-hidden="true" class="trigger-indicator" />
+        </button>
+      </nav>
+    </div>
+
+    <div class="focus:outline-none" {...$content("Identities")}>
       <Identities />
-    </TabPanel>
+    </div>
 
-    <TabPanel>
+    <div class="focus:outline-none" {...$content("Encrypt")}>
       <Encrypt />
-    </TabPanel>
+    </div>
 
-    <TabPanel>
+    <div class="focus:outline-none" {...$content("Decrypt")}>
       <Decrypt />
-    </TabPanel>
-  </Tabs>
+    </div>
+  </div>
 </main>
